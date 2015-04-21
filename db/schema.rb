c75257fb37c9c0e55ca21820c15bab86eb5c6548
AdrianChingHC <achc92@gmail.com>
2015-04-21 21:24:19 +0800

    Update gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420073836) do
+ActiveRecord::Schema.define(version: 20150421084239) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -30,36 +30,34 @@ ActiveRecord::Schema.define(version: 20150420073836) do
   add_index "comments", ["request_id"], name: "index_comments_on_request_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
-  create_table "identities", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.string   "provider",   limit: 255
-    t.string   "uid",        limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
-  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
-
   create_table "images", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
 
-  create_table "requests", force: :cascade do |t|
-    t.string   "title",              limit: 255
-    t.string   "matric_no",          limit: 255
-    t.string   "phone_no",           limit: 255
-    t.string   "rate",               limit: 255
-    t.text     "description",        limit: 65535
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.integer  "user_id",            limit: 4
-    t.integer  "category_id",        limit: 4
+  create_table "pictures", force: :cascade do |t|
+    t.integer  "request_id",         limit: 4
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.string   "image_file_name",    limit: 255
     t.string   "image_content_type", limit: 255
     t.integer  "image_file_size",    limit: 4
     t.datetime "image_updated_at"
+  end
+
+  add_index "pictures", ["request_id"], name: "index_pictures_on_request_id", using: :btree
+
+  create_table "requests", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.string   "matric_no",   limit: 255
+    t.string   "phone_no",    limit: 255
+    t.string   "rate",        limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "user_id",     limit: 4
+    t.integer  "category_id", limit: 4
   end
 
   create_table "users", force: :cascade do |t|
@@ -99,5 +97,5 @@ ActiveRecord::Schema.define(version: 20150420073836) do
 
   add_foreign_key "comments", "requests"
   add_foreign_key "comments", "users"
-  add_foreign_key "identities", "users"
+  add_foreign_key "pictures", "requests"
 end
