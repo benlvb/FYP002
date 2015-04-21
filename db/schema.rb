6f784917e77d543922e18ccdb4d53bc444c643a3
AdrianChingHC <achc92@gmail.com>
2015-04-21 14:30:40 +0800

    Clean up image field for paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421061649) do
+ActiveRecord::Schema.define(version: 20150421062732) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
