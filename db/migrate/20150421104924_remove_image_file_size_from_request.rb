223f598d430b65b36a9483618ad065648ad59df1
benlvb <benliewvb@gmail.com>
2015-05-05 13:26:39 +0800

    Delete migration file

--- db/migrate/20150421104924_remove_image_file_size_from_request.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageFileSizeFromRequest < ActiveRecord::Migration
-  def change
-    remove_column :requests, :image_file_size, :integer
-  end
-end
--- db/migrate/20150421104952_remove_image_updated_at_from_request.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageUpdatedAtFromRequest < ActiveRecord::Migration
-  def change
-    remove_column :requests, :image_updated_at, :datetime
-  end
-end
