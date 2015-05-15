ee116414e95f06aae120620bdf6a3d8bfd4d2e34
benlvb <benliewvb@gmail.com>
2015-05-15 18:30:00 +0800

    Revert a08b925..4cc562b
    This rolls back to commit a08b9254d27c16e09a82792f6fcd50142067e229.

--- config/deploy/staging.rb
+++
@@ -1,5 +0,0 @@
-set :user, 'deploy'
-server '128.199.231.145', user: fetch(:user), roles: %w{web app db}
-set :application, 'fyp_staging'
-set :rails_env, 'staging'
-set :release_note_url, 'http://staging.fyp.com/release_note.txt'
