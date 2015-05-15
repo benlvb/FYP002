e06365e24b27006be22e38cb3efac2e86c842dd4
benlvb <benliewvb@gmail.com>
2015-05-15 17:03:11 +0800

    Add secrets.yml to link file

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,10 +20,10 @@ set :deploy_to, '/var/www/fyp_staging'
 # set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
