3fad43e2e331b200eb8d221852ac52aed6f7689b
benlvb <benliewvb@gmail.com>
2015-05-15 16:58:29 +0800

    Configure deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :deploy_to, '/var/www/fyp_staging'
 # set :log_level, :debug
 
 # Default value for :pty is false
-set :pty, true
+# set :pty, true
 
 # Default value for :linked_files is []
 set :linked_files, %w{config/database.yml}
