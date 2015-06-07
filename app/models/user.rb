f1059eb9a2e804a200bc0d3c0f0bce23e532c3eb
benlvb <benliewvb@gmail.com>
2015-06-07 15:53:12 +0800

    Add destroy dependency

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :confirmable
 
-  has_many :requests
-  has_many :comments
+  has_many :requests, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
 
   validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
 
